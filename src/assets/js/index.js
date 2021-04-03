f970bff283c4d79067f44886419d7cfe1be5481c
Augustine <ashramwen@gmail.com>
2021-04-03 16:39:33 +0800

    feat: make Home page

--- src/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-new Splide('.splide', {
-  type: 'loop',
-  padding: {
-    right: '5rem',
-    left: '5rem',
-  },
-}).mount();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,41 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.html', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          50: '#F6F6F6',
+          100: '#f5F5F5',
+          200: '#E8E8E8',
+          300: '#DBDBDB',
+          400: '#929292',
+          500: '#4C4C4C',
+        },
+        orange: {
+          light: '#F7C352',
+          DEFAULT: '#FB9C44',
+          dark: '#F08441',
+        },
+      },
+      fontSize: {
+        14: '1.4rem',
+        16: '1.6rem',
+        66: '16.5rem',
+      },
+      height: {
+        '3px': '3px',
+      },
+      margin: {
+        18: '4.5rem',
+      },
+      width: {
+        68: '17rem',
+        66: '16.5rem',
+      },
+    },
   },
   variants: {
     extend: {},
